3201b9219bdf9179f14aa3ef4f92db3f790d0afe
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2022-04-05 10:37:31 +0300

    invoice page

--- src/app/components/pages/invoice/app.invoice.component.ts
+++ src/app/components/pages/invoice/app.invoice.component.ts
@@ -3,7 +3,28 @@ import {BreadcrumbService} from 'src/app/service/app.breadcrumb.service';
 import {AppLayoutComponent} from 'src/app/app.layout.component';
 
 @Component({
-    templateUrl: './app.invoice.component.html'
+    templateUrl: './app.invoice.component.html',
+    styles: [`
+        :host ::ng-deep .p-datatable-lg table { 
+            border-collapse: separate !important;
+            border-spacing: 0 1rem !important;
+            
+            td, th {
+                border: none;
+            }
+        }
+
+        :host ::ng-deep .p-datatable.p-datatable-lg .p-datatable-footer {
+            margin-top: 3rem;
+            margin-bottom: 1rem;
+            padding: 0 !important;
+            border: none !important;
+        }
+
+        :host ::ng-deep .p-datatable table > thead > tr {
+            background-color: black !important;
+        }
+    `]
 })
 export class AppInvoiceComponent {
 
@@ -16,4 +37,11 @@ export class AppInvoiceComponent {
     print() {
         window.print();
     }
+
+    tableData = [
+        {name: 'Creative Design', date: '29 Jul 2021', hours: '80h', rate: '$40.00', amount: '$920.34'},
+        {name: 'Logo Design', date: '29 Jul 2021', hours: '10h', rate: '$20.00', amount: '$834.12'},
+        {name: 'Web Development', date: '29 Jul 2021', hours: '7h', rate: '$60.00', amount: '$340.20'},
+        {name: 'Logo Design', date: '29 Jul 2021', hours: '72h', rate: '$20.00', amount: '$834.12'}
+    ]
 }

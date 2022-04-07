b930b39f402e5ba10bbb6e59c317ee34493f1e9a
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2022-04-07 09:39:41 +0300

    add timeline

--- src/app/components/pages/timeline/app.timelinedemo.component.ts
+++ src/app/components/pages/timeline/app.timelinedemo.component.ts
@@ -1,56 +1,12 @@
-import {Component, OnInit} from '@angular/core';
-import {BreadcrumbService} from 'src/app/service/app.breadcrumb.service';
-import {PrimeIcons} from 'primeng/api';
+import { Component } from '@angular/core';
+import { BreadcrumbService } from 'src/app/service/app.breadcrumb.service';
 
 @Component({
     templateUrl: './app.timelinedemo.component.html',
-    styles: [`
-        .custom-marker {
-            display: flex;
-            width: 2rem;
-            height: 2rem;
-            align-items: center;
-            justify-content: center;
-            color: #ffffff;
-            border-radius: 50%;
-            z-index: 1;
-        }
-        
-        ::ng-deep {
-            .p-timeline-event-content,
-            .p-timeline-event-opposite {
-                line-height: 1;
-            }
-        }
-        
-        @media screen and (max-width: 960px) {
-            :host ::ng-deep {
-                .customized-timeline {
-                    .p-timeline-event:nth-child(even) {
-                        flex-direction: row !important;
-        
-                        .p-timeline-event-content {
-                            text-align: left !important;
-                        }
-                    }
-        
-                    .p-timeline-event-opposite {
-                        flex: 0;
-                    }
-        
-                    .p-card {
-                        margin-top: 1rem;
-                    }
-                }
-            }
-        } 
-    `]
 })
-export class AppTimelineDemoComponent implements OnInit{
+export class AppTimelineDemoComponent {
 
-    customEvents: any[];
-
-    horizontalEvents: any[];
+    selectedIndex: number;
 
     constructor(private breadcrumbService: BreadcrumbService) {
         this.breadcrumbService.setItems([
@@ -58,22 +14,12 @@ export class AppTimelineDemoComponent implements OnInit{
         ]);
     }
 
-    ngOnInit() {
-        this.customEvents = [
-            {
-                status: 'Ordered',
-                date: '15/10/2020 10:30',
-                icon: PrimeIcons.SHOPPING_CART,
-                color: '#9C27B0',
-                image: 'game-controller.jpg'
-            },
-            {status: 'Processing', date: '15/10/2020 14:00', icon: PrimeIcons.COG, color: '#673AB7'},
-            {status: 'Shipped', date: '15/10/2020 16:15', icon: PrimeIcons.ENVELOPE, color: '#FF9800'},
-            {status: 'Delivered', date: '16/10/2020 10:00', icon: PrimeIcons.CHECK, color: '#607D8B'}
-        ];
-
-        this.horizontalEvents = [
-            '2020', '2021', '2022', '2023'
-        ];
-    }
+    timelineContent = [
+        {icon: 'pi pi-fw pi-book', title: 'Breakfast', info:'Aliquam diam vitae luctus tempor.'},
+        {icon: 'pi pi-fw pi-sun', title: 'Morning', info:'Placerat quis fames aenean cursus commodo morbi.'},
+        {icon: 'pi pi-fw pi-thumbs-up', title: 'Lunch', info:'Nec egestas mauris tincidunt tristique ut cum.'},
+        {icon: 'pi pi-fw pi-video', title: 'Afternoon', info:'Nec egestas mauris tincidunt tristique ut cum.'},
+        {icon: 'pi pi-fw pi-th-large', title: 'Supper', info:'Nec egestas mauris tincidunt tristique ut cum.'},
+        {icon: 'pi pi-fw pi-link', title: 'Evening', info:'Nec egestas mauris tincidunt tristique ut cum.'},
+    ];
 }
--- src/app/components/pages/timeline/app.timelinedemo.module.ts
+++ src/app/components/pages/timeline/app.timelinedemo.module.ts
@@ -2,15 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppTimelineDemoRoutingModule } from './app.timelinedemo-routing.module';
 import { AppTimelineDemoComponent } from './app.timelinedemo.component';
-import { TimelineModule } from 'primeng/timeline';
-import { CardModule } from 'primeng/card';
-
 @NgModule({
   imports: [
     CommonModule,
-    AppTimelineDemoRoutingModule,
-    CardModule,
-    TimelineModule
+    AppTimelineDemoRoutingModule
   ],
   declarations: [AppTimelineDemoComponent]
 })

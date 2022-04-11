37d166d4491e1ea4afedf0b0c647ef95e90366aa
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2022-04-11 14:21:54 +0300

    add product overview

--- src/app/components/pages/ecommerce/productoverview/productoverview.component.ts
+++ src/app/components/pages/ecommerce/productoverview/productoverview.component.ts
@@ -1,14 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { BreadcrumbService } from 'src/app/service/app.breadcrumb.service';
 
 @Component({
-  selector: 'app-productoverview',
-  templateUrl: './productoverview.component.html',
+    selector: 'app-productoverview',
+    templateUrl: './productoverview.component.html',
 })
 export class ProductOverviewComponent implements OnInit {
+    
+    color: string = 'bluegray';
 
-  constructor() { }
+    size: string = 'M';
 
-  ngOnInit(): void {
-  }
+    liked: boolean;
 
+    images: string[];
+
+    selectedImageIndex: number = 0;
+
+    quantity: number = 1;
+      
+    constructor(private breadcrumbService: BreadcrumbService) {
+      this.breadcrumbService.setItems([
+        {label: 'Product Overview'},
+      ]);
+    }
+    ngOnInit(): void {
+
+      this.images = [
+          'product-overview-3-1.png',
+          'product-overview-3-2.png',
+          'product-overview-3-3.png',
+          'product-overview-3-4.png'
+      ];
+    }
 }

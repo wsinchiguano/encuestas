a36d221e90faa7e3bf27d4a0b8dc650b67c54e54
yigitfindikli <yigitfindikli@gmail.com>
2022-04-18 14:49:27 +0300

    Forgot password added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -20,6 +20,7 @@ import {AppLayoutComponent} from './app.layout.component';
             {path: 'error', loadChildren: () => import('./components/pages/error/app.error.module').then(m => m.AppErrorModule)},
             {path: 'access', loadChildren: () => import('./components/pages/accessdenied/app.accessdenied.module').then(m => m.AppAccessdeniedModule)},
             {path: 'login', loadChildren: () => import('./components/pages/login/app.login.module').then(m => m.AppLoginModule)},
+            {path: 'forgotpassword', loadChildren: () => import('./components/pages/forgotpassword/app.forgotpassword.module').then(m => m.AppForgotPasswordModule)},
             {path: 'notfound', loadChildren: () => import('./components/pages/notfound/app.notfound.module').then(m => m.AppNotfoundModule)},
             {path: '**', redirectTo: '/notfound'},
         ], {scrollPositionRestoration: 'enabled'})

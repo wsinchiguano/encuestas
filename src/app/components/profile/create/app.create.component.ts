942927a341eea5f146c03afa6a566453f8b4fadb
Şeyma Nur Güllü <67110171+seymanurgullu@users.noreply.github.com>
2022-04-25 15:46:15 +0300

    create-page-profile added

--- src/app/components/profile/create/app.create.component.ts
+++ src/app/components/profile/create/app.create.component.ts
@@ -1,14 +1,24 @@
-import {Component} from '@angular/core';
-import {BreadcrumbService} from 'src/app/service/app.breadcrumb.service';
+import { Component } from '@angular/core';
+import { BreadcrumbService } from 'src/app/service/app.breadcrumb.service';
+
+interface Create {
+    name: string;
+}
 
 @Component({
     templateUrl: './app.create.component.html'
 })
 export class AppCreateComponent {
-    
+
+    pushNotifacitons;
+    inStock;
+    smsNotifications;
+
     constructor(private breadcrumbService: BreadcrumbService) {
         this.breadcrumbService.setItems([
-            {label: 'Create'}
+            { label: 'Create' }
         ]);
     }
+
+
 }
--- src/app/components/profile/create/app.create.module.ts
+++ src/app/components/profile/create/app.create.module.ts
@@ -2,11 +2,52 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppCreateRoutingModule } from './app.create-routing.module';
 import { AppCreateComponent } from './app.create.component';
+import { ButtonModule } from 'primeng/button';
+import { CascadeSelectModule } from 'primeng/cascadeselect';
+import { CheckboxModule } from 'primeng/checkbox';
+import { ChipModule } from 'primeng/chip';
+import { ColorPickerModule } from 'primeng/colorpicker';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputMaskModule } from 'primeng/inputmask';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { InputSwitchModule } from 'primeng/inputswitch';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { KnobModule } from 'primeng/knob';
+import { ListboxModule } from 'primeng/listbox';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { RatingModule } from 'primeng/rating';
+import { SelectButtonModule } from 'primeng/selectbutton';
+import { SliderModule } from 'primeng/slider';
+import { ToggleButtonModule } from 'primeng/togglebutton';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   imports: [
     CommonModule,
-    AppCreateRoutingModule
+    AppCreateRoutingModule,
+    DropdownModule,
+    InputMaskModule,
+    InputNumberModule,
+    ColorPickerModule,
+    CascadeSelectModule,
+    MultiSelectModule,
+    ToggleButtonModule,
+    SliderModule,
+    InputTextareaModule,
+    RadioButtonModule,
+    InputTextModule,
+    RatingModule,
+    ChipModule,
+    KnobModule,
+    InputSwitchModule,
+    ListboxModule,
+    SelectButtonModule,
+    CheckboxModule,
+    ButtonModule,
+    FormsModule,
   ],
   declarations: [AppCreateComponent]
 })

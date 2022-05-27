2b34b23378261f16e2ab2fd8e9efb71b69e93876
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2022-05-27 14:16:54 +0300

    Mail | Add sidebar

--- src/app/components/apps/mail/apps.mail.module.ts
+++ src/app/components/apps/mail/apps.mail.module.ts
@@ -6,11 +6,17 @@ import { InboxComponent } from './inbox/inbox.component';
 import { ComposeComponent } from './compose/compose.component';
 import { DetailComponent } from './detail/detail.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { MenuModule } from 'primeng/menu';
+import { ButtonModule } from 'primeng/button'
+import { RippleModule } from 'primeng/ripple'
 
 @NgModule({
   imports: [
     CommonModule,
-    AppsMailRoutingModule
+    AppsMailRoutingModule,
+    MenuModule,
+    ButtonModule,
+    RippleModule
   ],
   declarations: [AppsMailComponent, InboxComponent, ComposeComponent, DetailComponent, SidebarComponent]
 })
--- src/app/components/apps/mail/sidebar/sidebar.component.ts
+++ src/app/components/apps/mail/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
     selector: 'mail-sidebar',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
+    
+    activeIndex: number = 0;
+
+    items: MenuItem[];
 
     constructor() { }
 
     ngOnInit(): void {
+        this.items = [
+            {label: 'Inbox', icon: 'pi pi-inbox', badge: '1100'},
+            {label: 'Starred', icon: 'pi pi-star'},
+            {label: 'Spam', icon: 'pi pi-ban'},
+            {label: 'Important', icon: 'pi pi-bookmark'},
+            {label: 'Sent', icon: 'pi pi-send'},
+            {label: 'Drafts', icon: 'pi pi-file'},
+            {label: 'Trash', icon: 'pi pi-trash'}
+        ];
     }
-
 }

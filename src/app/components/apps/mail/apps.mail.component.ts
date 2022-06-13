e54686209157fbcfc0e16909510ec79fb093b4db
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2022-06-13 13:06:27 +0300

    Mail | Fix animation & refactore

--- src/app/components/apps/mail/apps.mail.component.ts
+++ src/app/components/apps/mail/apps.mail.component.ts
@@ -1,31 +1,29 @@
 import { Component, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { MailService } from 'src/app/service/mailservice';
-import { animate, style, transition, trigger, state } from "@angular/animations";
+import { animate, style, transition, trigger, animation, useAnimation } from "@angular/animations";
+
+const showAnimation = animation([
+    style({ transform: 'translateX(-100%)', opacity: 0 }),
+    animate('150ms cubic-bezier(0, 0, 0.2, 1)')
+]);
+
+const hideAnimation = animation([
+    animate('150ms cubic-bezier(0, 0, 0.2, 1)', style({ transform: 'translateX(-100%)', opacity: 0 }))
+]);
 
 @Component({
     selector: 'app-apps.mail',
     templateUrl: './apps.mail.component.html',
     styleUrls: [`./apps.mail.component.scss`],
     animations: [
-        trigger('slideInOut', [  
-          state(
-            'in',
-            style({
-                transform: 'translateX(0)',
-                width: '300px',
-                opacity: 1,
-            })
-          ),
-          state(
-            'out',
-            style({
-                transform: 'translateX(-100%)',
-                width: 0,
-                opacity: 0
-            })
-          ),
-          transition('in <=> out', animate('0.4s cubic-bezier(0.05, 0.74, 0.2, 0.99)')),
+        trigger('panelState', [
+            transition('void => visible', [
+                useAnimation(showAnimation)
+            ]),
+            transition('visible => void', [
+                useAnimation(hideAnimation)
+            ])
         ])
     ],
 })
@@ -35,10 +33,6 @@ export class AppsMailComponent implements OnDestroy {
 
     sidebarState: boolean;
 
-    sidebarClosed: boolean = false;
-
-    timeout;
-    
     constructor(private mailService: MailService){
         this.sidebarSubscription = this.mailService.sidebarState$.subscribe(state => {
             this.sidebarState = state
@@ -46,17 +40,7 @@ export class AppsMailComponent implements OnDestroy {
         });
     }
 
-    onAnimationStart(event) {
-        if (event.fromState === 'in' && event.toState === 'out') {
-            this.timeout = setTimeout(() => {this.sidebarClosed = true}, 80)
-        }
-        if (event.fromState === 'out' && event.toState === 'in') {
-            this.sidebarClosed = false;
-        }
-    }
-
     ngOnDestroy(): void {
-        clearTimeout(this.timeout);
         this.sidebarSubscription.unsubscribe();
     }
 }

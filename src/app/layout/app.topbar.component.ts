23fa0471e6b5b0b50461e133a16301b015ad4fa0
Şeyma Nur Güllü <67110171+seymanurgullu@users.noreply.github.com>
2022-09-14 12:13:22 +0300

    refactored topbar user menu

--- src/app/layout/app.topbar.component.ts
+++ src/app/layout/app.topbar.component.ts
@@ -15,34 +15,7 @@ export class AppTopbarComponent implements OnInit {
     constructor(public layoutService: LayoutService, public el: ElementRef) { }
 
     ngOnInit(): void {
-        /*
-        this.userMenuItems = [
-            {
-                icon: 'pi pi-user',
-                label: 'Profile',
-                styleClass: 'border-round overflow-hidden'
-            },
-            {
-                icon: 'pi pi-cog',
-                label: 'Settings',
-                styleClass: 'border-round overflow-hidden'
-            },
-            {
-                icon: 'pi pi-calendar',
-                label: 'Calendar',
-                styleClass: 'border-round overflow-hidden'
-            },
-            {
-                icon: 'pi pi-inbox',
-                label: 'Inbox',
-                styleClass: 'border-round overflow-hidden'
-            },
-            {
-                icon: 'pi pi-power-off',
-                label: 'Logout',
-                styleClass: 'border-round overflow-hidden'
-            }
-        ];*/
+
     }
 
     onMenuButtonClick() {
